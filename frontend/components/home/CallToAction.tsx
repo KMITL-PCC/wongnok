a33f3e207c1689feb191179547d8dc4f88570fa0
Thanawat Bunprasob <zeroznice.nice@gmail.com>
2025-09-04 14:40:03 +0700

    ChageHomepage

--- frontend/components/home/CallToAction.tsx
+++ frontend/components/home/CallToAction.tsx
@@ -1,14 +1,19 @@
+// src/app/CallToAction.tsx
 import React from "react";
 import { Button } from "@/components/ui/button";
-import { ArrowRight, Icon } from "lucide-react";
+import { ArrowRight } from "lucide-react";
 
 const CallToAction = () => {
   return (
-    <div>
-      {/* ปุ่มกด */}
-      <Button className="absolute top-[500px] left-[590px] h-[47.6px] w-[250px] rounded-[40px] bg-green-500 text-sm font-bold text-white hover:bg-gray-800">
+    // This button is positioned absolutely relative to its parent container in the Hero component.
+    // It will appear centered below the desktop image gallery.
+    <div className="absolute top-120 left-1/2 -translate-x-1/2 -translate-y-1/2 transform">
+      <Button className="group inline-flex h-auto items-center rounded-full bg-green-500 px-6 py-2 text-sm font-bold text-white shadow-lg transition-transform hover:scale-105 hover:bg-green-600">
         <span>LOOK FOR FOOD</span>
-        <ArrowRight size={18} className="ml-1" />
+        <ArrowRight
+          size={18}
+          className="ml-2 transition-transform duration-300 group-hover:translate-x-1"
+        />
       </Button>
     </div>
   );
