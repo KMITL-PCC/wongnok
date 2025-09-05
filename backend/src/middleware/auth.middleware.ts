d1bed1b331657de42d6ea679e1d3730e59e0ceaf
thePeet25 <pbuythiy25@gmail.com>
2025-09-05 22:21:02 +0700

    refactor!: refactor docker-compose and refactor prisma cilent
    BREAKPOINT: change prisma output in schema
    
    - change docker file in backend
    - change prisma client output in schema
    - change every prisma client import

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -1,46 +1,59 @@
-import { Request, Response, NextFunction } from 'express';
-import passport from '../config/passport'; 
-import { Role, User } from '../../generated/prisma/client';
+import { Request, Response, NextFunction } from "express";
+import passport from "../config/passport";
+import { Role, User } from "@prisma/client";
 
 // --- Authentication Middleware ---
 // ตรวจสอบว่าผู้ใช้ล็อกอินอยู่หรือไม่
-export function isAuthenticated(req: Request, res: Response, next: NextFunction) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    // ส่ง Unauthorized response หรือ redirect ไปหน้า login
-    res.status(401).json({ message: 'Unauthorized: Please log in.' });
+export function isAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  // ส่ง Unauthorized response หรือ redirect ไปหน้า login
+  res.status(401).json({ message: "Unauthorized: Please log in." });
 }
 
 // ตรวจสอบ Role ของผู้ใช้
 export function hasRole(role: Role) {
-    return (req: Request, res: Response, next: NextFunction) => {
-        if (!req.isAuthenticated()) {
-            return res.status(401).json({ message: 'Unauthorized: Please log in.' });
-        }
-        // ตรวจสอบ role ของผู้ใช้ที่ล็อกอิน
-       const user = req.user as User;
-        if (user && user.role === role) {
-            return next();
-        }
-        res.status(403).json({ message: 'Forbidden: You do not have the necessary permissions.' });
-    };
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!req.isAuthenticated()) {
+      return res.status(401).json({ message: "Unauthorized: Please log in." });
+    }
+    // ตรวจสอบ role ของผู้ใช้ที่ล็อกอิน
+    const user = req.user as User;
+    if (user && user.role === role) {
+      return next();
+    }
+    res
+      .status(403)
+      .json({
+        message: "Forbidden: You do not have the necessary permissions.",
+      });
+  };
 }
 
 export function isLogedIn(req: Request, res: Response, next: NextFunction) {
-    if (!req.isAuthenticated()) {
-        return next();
-    } else {
-        res.status(401).json({ message: 'you are logged in , pls log out'})
-    }
+  if (!req.isAuthenticated()) {
+    return next();
+  } else {
+    res.status(401).json({ message: "you are logged in , pls log out" });
+  }
 }
 
-export function invalidCsrf(err: any, req: Request, res: Response, next: NextFunction) {
-    if (err.code === 'EBADCSRFTOKEN') {
-        return res.status(403).json({
-            message: 'Invalid CSRF token. Please refresh the page and try again.'
-        });
-    }
+export function invalidCsrf(
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (err.code === "EBADCSRFTOKEN") {
+    return res.status(403).json({
+      message: "Invalid CSRF token. Please refresh the page and try again.",
+    });
+  }
 
-    next(err);
+  next(err);
 }
