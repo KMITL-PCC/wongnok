d40b7a161ad5a061db2b326b2f4b237bfb3380da
Onlyakr <onlyakr2477@gmail.com>
2025-09-09 15:36:51 +0700

    fix: restaurant detail fetch

--- frontend/app/restaurants/[id]/page.tsx
+++ frontend/app/restaurants/[id]/page.tsx
@@ -58,22 +58,22 @@ const restaurantInfo = {
   services: ["delivery"],
 };
 
-// const getRestaurantById = async (id: string) => {
-//   try {
-//     const res = await fetch(
-//       `${process.env.NEXT_PUBLIC_BACKEND_URL}/restaurant/get/${id}`,
-//       {
-//         credentials: "include",
-//       },
-//     );
-//     if (!res.ok) {
-//       throw new Error("Failed to fetch restaurant" + res.status);
-//     }
-//     return res.json();
-//   } catch (error) {
-//     throw new Error("Failed to fetch restaurant" + error);
-//   }
-// };
+const getRestaurantById = async (id: string) => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/restaurant/get/${id}`,
+      {
+        credentials: "include",
+      },
+    );
+    if (!res.ok) {
+      throw new Error("Failed to fetch restaurant" + res.status);
+    }
+    return res.json();
+  } catch (error) {
+    throw new Error("Failed to fetch restaurant" + error);
+  }
+};
 
 const RestaurantDetailPage = async ({
   params,
